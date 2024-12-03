1b35c4732585c81e07964f53e69a6ecf178d26f6
guleao <guztavusz@gmail.com>
2024-12-03 18:30:27 -0300

    Criando jspringboot

---
+++ src/main/java/com/example/projeto4_augusto/Projeto4AugustoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto4_augusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto4AugustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto4AugustoApplication.class, args);
+	}
+
+}
